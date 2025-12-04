550d99f4db3fc73d6686d76927f88b1a1291ddea
super <97206982+SuperMo0@users.noreply.github.com>
2025-12-04 20:27:39 +0200

    add Database

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require('express');
 const { log } = require('node:console');
 const app = express();
 const path = require('node:path');
+const connectionString = process.env.DATABASE_URL;
+const pg = require('pg');
+const client = new pg.Client(connectionString);
 
+client.connect().then(() => {
+    console.log('connected to data base');
+})
 
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
@@ -10,13 +16,26 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 let data = require('./data.js');
 
-app.get('/', (req, res) => {
+async function getAll() {
+    let result = await client.query('select * from messages');
+    return result.rows;
+}
+app.get('/', async (req, res) => {
+    let data = await getAll();
     res.render('index.ejs', { data });
 });
 
-app.post('/', (req, res) => {
+async function insertMessage(name, message) {
+    const sql = `
+    insert into messages (username,message) values($1,$2)
+    `
+    try { client.query(sql, [name, message]); }
+    catch (err) { log(err); }
+}
+app.post('/', async (req, res) => {
     console.log('recived new message');
-    data.push({ name: req.body.name, message: req.body.message, data: 'today' });
+    log(req.body);
+    await insertMessage(req.body.name, req.body.message);
     res.redirect('/');
 })
 
